689d370c0bb5cbe7766740af4b7030c452ca7875
gilladi <adityagill10@gmail.com>
2024-03-05 17:37:51 -0500

    Completed ChangeType

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,29 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [questionType, setQuestionType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+
+    function toggleQuestionType(): void {
+        setQuestionType(
+            questionType === "multiple_choice_question"
+                ? "short_answer_question"
+                : "multiple_choice_question"
+        );
+    }
+
+    return (
+        <div>
+            <div>
+                Current question type:{" "}
+                <span>
+                    {questionType === "multiple_choice_question"
+                        ? "Multiple Choice"
+                        : "Short Answer"}
+                </span>
+                <Button onClick={toggleQuestionType}>Change Type</Button>
+            </div>
+        </div>
+    );
 }
